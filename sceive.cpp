83a46fa597f361f8ff3e787f2ef304f8688a00dd
ChinnamPoojithaSuryaRajeswari <119870581+ChinnamPoojithaSuryaRajeswari@users.noreply.github.com>
2024-02-17 17:50:40 +0530

    Update sceive.cpp

--- sceive.cpp
+++ sceive.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 int main()
 {
-	int n;
+	long long n;
 	cin>>n;
 	if(n==0 or n==1){
-        return false;
+        cout<<-1;
     }
+    else{
 	vector<int>v(n+1,1);
 	v[0]=0;
 	v[1]=0;
     v[n]=0;
-	int j,p=0;
-	for(int i=2;i*i<=(n);i++)
+	int j;
+	for(int i=2;i<=int(sqrt(n));i++)
 	{
-		j=i*i;
+		j=i+i;
 		if(v[i]==1)
 		{
 		while(j<=n)
 		{
-			v[j+i]=0;
+			v[j]=0;
 			j+=i;
-			p+=1;
 		}
 	}
     }
@@ -31,5 +31,6 @@ int main()
     	{
     		cout<<i<<" ";
 		}
+	}
 	}
  }
